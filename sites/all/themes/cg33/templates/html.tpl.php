686e2bdcd4214db3326973f7afeb1b7ebb4704a3
lara <ebiz@larour-ebiz>
2014-04-04 14:47:02 +0300

    modif

--- sites/all/themes/cg33/templates/html.tpl.php
+++ sites/all/themes/cg33/templates/html.tpl.php
@@ -1,4 +1,4 @@
-<?php print $doctype;global $base_path; ?>
+<?php print $doctype;global $base_path; echo arg(0); ?>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
